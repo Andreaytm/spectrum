04cc2558f97b3f1d8ba9e8abfb5375795c305f25
Andrea Ma <andrea.ma@hotmail.co.uk>
2018-11-24 17:04:01 +0000

    Add sorting mechanism to sort through products in shop

--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url 
-from .views import do_search, filter_product_name
+from .views import do_search, filter_product_name, sort_product_a_z, sort_product_z_a, sort_asc_cost, sort_desc_cost
 
 urlpatterns =[
     url(r'^$', do_search, name='search'),
-    url(r'^reviews/$', filter_product_name, name='filter')
+    url(r'^reviews/$', filter_product_name, name='filter'),
+    url(r'^sort-a-z/$', sort_product_a_z, name='sort-a-z'),
+    url(r'^sort-z-a/$', sort_product_z_a, name='sort-z-a'),
+    url(r'^sort-asc-cost/$', sort_asc_cost, name='sort-asc-cost'),
+    url(r'^sort-desc-cost/$', sort_desc_cost, name='sort-desc-cost'),
     ]
--- search/views.py
+++ search/views.py
@@ -10,3 +10,19 @@ def filter_product_name(request):
     products = Product.objects.all()
     review = Review.objects.filter(product__name__icontains=request.GET['z'])
     return render(request, "reviewposts.html", {"review" : review, "products" : products})
+
+def sort_product_a_z(request):
+    products = Product.objects.all().order_by('name')
+    return render(request, "products.html", {"products" : products})
+
+def sort_product_z_a(request):
+    products = Product.objects.all().order_by('-name')
+    return render(request, "products.html", {"products" : products})
+
+def sort_asc_cost(request):
+    products = Product.objects.all().order_by('price')
+    return render(request, "products.html", {"products" : products})
+
+def sort_desc_cost(request):
+    products = Product.objects.all().order_by('-price')
+    return render(request, "products.html", {"products" : products})
