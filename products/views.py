2365376dde7357c1930e9dcd3661d3a3ea2f8f2e
Andrea Ma <andrea.ma@hotmail.co.uk>
2018-12-02 17:41:45 +0000

    Update and include filter option of product sizes

--- products/views.py
+++ products/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Product
 
-# Create your views here.
 def all_products(request):
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('name')
     return render(request, 'products.html', {"products": products})
