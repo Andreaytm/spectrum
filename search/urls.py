7c40cedf6e871b8773774f5ee8a6893e65520f02
Andrea Ma <andrea.ma@hotmail.co.uk>
2018-12-04 22:58:34 +0000

    Add ability to sort and filter products

--- search/urls.py
+++ search/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url 
-from .views import do_search, filter_product_name, sort_product_a_z, sort_product_z_a, sort_asc_cost, sort_desc_cost
+from .views import do_search, filter_product_name, sort_product_a_z, sort_product_z_a, sort_asc_cost, sort_desc_cost, filter_product_a2, filter_product_a3, filter_product_a4
 
 urlpatterns =[
     url(r'^$', do_search, name='search'),
@@ -8,4 +8,7 @@ urlpatterns =[
     url(r'^sort-z-a/$', sort_product_z_a, name='sort-z-a'),
     url(r'^sort-asc-cost/$', sort_asc_cost, name='sort-asc-cost'),
     url(r'^sort-desc-cost/$', sort_desc_cost, name='sort-desc-cost'),
+    url(r'^filter_product_a4/$', filter_product_a4, name='filter_product_a4'),
+    url(r'^filter_product_a3/$', filter_product_a3, name='filter_product_a3'),
+    url(r'^filter_product_a2/$', filter_product_a2, name='filter_product_a2')
     ]
--- search/views.py
+++ search/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from products.models import Product
 from review.models import Review
 
+
 def do_search(request):
     products = Product.objects.filter(description__icontains=request.GET['q'])
     return render(request, "products.html", {"products" : products})
@@ -25,4 +26,16 @@ def sort_asc_cost(request):
 
 def sort_desc_cost(request):
     products = Product.objects.all().order_by('-price')
+    return render(request, "products.html", {"products" : products})
+    
+def filter_product_a4(request):
+    products = Product.objects.filter(name__icontains='A4')
+    return render(request, "products.html", {"products" : products})
+
+def filter_product_a3(request):
+    products = Product.objects.filter(name__icontains='A3')
+    return render(request, "products.html", {"products" : products})
+
+def filter_product_a2(request):
+    products = Product.objects.filter(name__icontains='A2')
     return render(request, "products.html", {"products" : products})
