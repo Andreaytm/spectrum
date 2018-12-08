7b8a9fdac231fc729fb2d5a4943b4ceb1a31377d
Andrea Ma <andrea.ma@hotmail.co.uk>
2018-12-08 22:21:58 +0000

    Update scrollbtn for mobile and added css for new feats

--- products/models.py
+++ products/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 
-# Create your models here.
 class Product(models.Model):
+    
+    A4= 'A4'
+    A3= 'A3'
+    A2= 'A2'
+    
+    SIZE_CHOICES = (
+        (A4, 'A4'),
+        (A3, 'A3'),
+        (A2, 'A2'),)
+        
     name = models.CharField(max_length=254, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
-    
+    size = models.CharField(max_length=50, choices=SIZE_CHOICES, default='A4')
+    tags = models.CharField(max_length=254, default='')
+
     class Meta:
         ordering = ('name',)
         
---
+++ static/js/scrollbtn.js
@@ -0,0 +1,18 @@
+/* adapted from www.w3schools.com */
+window.onscroll = function () {
+    scrollToTop()
+};
+
+function scrollToTop() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        document.getElementById("toTopBtn").style.display = "block";
+    } else {
+        document.getElementById("toTopBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on button scroll to the top of the page
+function GoToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
